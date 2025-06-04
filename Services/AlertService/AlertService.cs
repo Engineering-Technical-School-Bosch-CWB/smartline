8abd3470a8f25172c21b8d51ac928bd127958b05
Donathan Gonçalves <donathanramalho@yahoo.com.br>
2025-06-04 14:06:37 -0300

    Adding Delete Button

--- Services/AlertService/AlertService.cs
+++ Services/AlertService/AlertService.cs
@@ -4,17 +4,28 @@ namespace SmartLine.Services.AlertService;
 
 public class AlertService : IAlertService
 {
-    public async Task ShowAlertAsync(string title, string message, string cancel)
+    public async Task ShowAlertAsync(string title, string message, string accept)
     {
         var mainPage = Application.Current?.Windows.FirstOrDefault()?.Page;
 
         if (mainPage != null)
         {
-            await mainPage.DisplayAlert(title, message, cancel);
+            await mainPage.DisplayAlert(title, message, accept);
         }
-        else
+
+        System.Diagnostics.Debug.WriteLine("No window available to show alert.");
+    }
+
+    public async Task<bool> ShowConfirmAsync(string title, string message, string accept, string cancel)
+    {
+        var mainPage = Application.Current?.Windows.FirstOrDefault()?.Page;
+
+        if (mainPage != null)
         {
-            System.Diagnostics.Debug.WriteLine("No window available to show alert.");
+            return await mainPage.DisplayAlert(title, message, accept, cancel);
         }
+
+        System.Diagnostics.Debug.WriteLine("No window available to show confirmation.");
+        return false;
     }
 }
--- Services/AlertService/IAlertService.cs
+++ Services/AlertService/IAlertService.cs
@@ -2,5 +2,6 @@ namespace SmartLine.Services.AlertService;
 
 public interface IAlertService
 {
-    Task ShowAlertAsync(string title, string message, string cancel);
+    Task ShowAlertAsync(string title, string message, string accept);
+    Task<bool> ShowConfirmAsync(string title, string message, string accept, string cancel);
 }
